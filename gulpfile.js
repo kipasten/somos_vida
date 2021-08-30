47b3fbd6eb4784d1ce6a1a255df143110138065d
Rafael Morales <rmorales@utem.cl>
2021-08-30 16:43:36 -0400

    Agregada minificacion de css

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require('sass'));
 var concat = require("gulp-concat");
+const cleanCSS = require('gulp-clean-css');
 gulp.task("sass", function () {
   return gulp
     .src("./sass/**/*.scss")
     .pipe(concat("main.min.scss"))
     .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest("./css/"));
 });
